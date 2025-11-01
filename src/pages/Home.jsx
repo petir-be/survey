36e39140e5234efa9c0e7f3e5d40822202a3304b
pits <pits@KURT>
2025-11-01 10:34:48 +0800

    create forms bullshit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import "../global.css";
 import DotShader from "../components/DotShader";
 import home1 from "/src/assets/2.svg";
@@ -7,15 +7,13 @@ import aboutus from "../assets/hugeicons_ai-dna.svg";
 import FAQ from "../components/FAQ";
 
 
-//testing
-
-//testing ulit
-//added testing package
-
 function Home() {
+const [showModal, setShowModal] = useState(false);
+
+
   return (
     <>
-      <div className="flex items-center flex-1 min-h-full bg-[var(--white)] z-10">
+      <div className="flex items-center justify-center flex-1 min-h-full bg-[var(--white)] z-10">
         <div className="w-2/7 h-dvh pt-25">
           <div className="m-12">
             <h1 className="font-vagrounded text-3xl mb-2">The Future</h1>
@@ -26,9 +24,10 @@ function Home() {
               architectures. The next era of predictive analytics starts here.
             </p>
           </div>
+          <button className="m-12 cursor-pointer" onClick={() => setShowModal(true)}>aa</button>
         </div>
         <div className="w-3/7 relative h-dvh overflow-hidden pt-15 border-2 border-[var(--dirty-white)] bg-[var(--white)] z-10">
-          <div className="absolute z-1 w-full">
+          <div className="absolute z-1 w-full h-full flex justify-center items-center">
             <img src={home1} alt="aa" width={800} height={800} />
           </div>
           <div className="absolute top-0 left-0 h-full w-full">
@@ -40,14 +39,93 @@ function Home() {
           </div>
         </div>
         <div className="justify-center flex flex-col gap-5 w-2/7 h-dvh pt-25 ">
-            <HomeBox title = "About us" icon={aboutus}/>
-            <HomeBox title = "About us" icon={aboutus}/>
-            <HomeBox title = "About us" icon={aboutus}/>
+            <HomeBox title="About us" icon={aboutus}/>
+            <HomeBox title="About us" icon={aboutus}/>
+            <HomeBox title="About us" icon={aboutus}/>
 
             <div className="flex justify-center">
                 <FAQ />
             </div>
         </div>
+        {showModal && (
+          <>
+            <div className="p-10 w-2/3 h-2/3 bg-(--white) ring ring-white rounded-lg fixed z-50">
+              <h1 className="font-vagrounded text-lg">Start a new Form</h1>
+              <div className="absolute top-0 right-0 w-15 h-15 flex items-center justify-center">
+                <button onClick={() => setShowModal(false)} className="w-full h-full cursor-pointer">X</button>
+              </div>
+              <div className="p-5 flex items-center justify-evenly w-full h-full">
+
+                {/* create own forms */}
+                <div className="flex flex-col gap-2 items-center w-full h-full">
+                  <span className="relative w-11/12 h-4/5 bg-white/20 shadow-md/20">
+                      
+                      {/* circle and rectangle */}
+                      <span className="inset-shadow-sm/30 w-5 h-5 absolute top-8 left-10 rounded-full"></span>
+                      <span className="inset-shadow-sm/30 w-5 h-5 absolute top-15 left-10 rounded-full"></span>
+                      <span className="w-5 h-5 inset-shadow-sm/30 absolute top-22 left-10 rounded-full"></span>
+
+                      <span className="w-7/12 h-3 inset-shadow-sm/30 absolute top-9 left-17"></span>
+                      <span className="w-4/12 h-3 inset-shadow-sm/30 absolute top-16 left-17"></span>
+                      <span className="w-2/12 h-3 inset-shadow-sm/30 absolute top-23 left-17"></span>
+
+                      {/* plus circle */}
+                      <span className="w-15 h-15 rounded-full inset-shadow-sm/30 absolute top-50 left-15 flex justify-center items-center">
+                        <span className="flex justify-center items-center w-11 h-11 rounded-full shadow-sm/30">
+                          <span className="w-8 h-2 inset-shadow-sm/30 rounded-lg flex justify-center items-center">
+                            {/* ayaw maalis pota */}
+                            <span className="mix-blend-lighten w-2 h-2 bg-white/30"></span> 
+                          </span>
+                          <span className="fixed w-2 h-8 inset-shadow-sm/30 rounded-lg flex justify-center items-center">
+                            <span className="mix-blend-lighten w-2 h-2 bg-white/30"></span>
+                          </span>
+                        </span>
+                      </span>
+
+                      
+                      <span className="absolute -bottom-4 right-12 shadow-[2px_0_5px_rgba(0,0,0,0.2)] z-1 w-2 h-34 rotate-45  "></span>
+                      <div
+                        className="absolute bottom-0 z-2 right-0 w-25 h-25 bg-linear-150 from-[#F9F9F9] to-[#CCCDD9]"
+                        style={{
+                          clipPath: 'polygon(0 0, 100% 0, 0 100%)',
+                          filter: 'drop-shadow(2px 2px 4px rgba(0, 0, 0, 1))'
+    
+                        }}
+                      />
+                    <div
+                    className="absolute z-0 -bottom-2 -right-2 w-28 h-29"
+                    style={{
+                      clipPath: 'polygon(100% 0, 0 100%, 100% 100%)',
+                      background: '#DFE0F0'
+                      // background:'#000000'
+                    }}
+                  />
+
+                  </span>
+                  Create you own forms
+                  </div>
+                  
+                  {/* generate with ai */}
+                <div className="flex flex-col gap-2 items-center w-full h-full">
+                  <span className="relative w-11/12 h-4/5 bg-white/20 shadow-md/20">
+                      
+                  
+                  </span>
+                  Create you own forms
+                  </div>
+
+                  {/* Use a template */}
+                  <div className="flex flex-col gap-2 items-center w-full h-full">
+                  <span className="relative w-11/12 h-4/5 bg-white/20 shadow-md/20">
+                      
+                    
+                  </span>
+                  Create you own forms
+                  </div>
+              </div>
+            </div>
+          </>
+        )}
       </div>
     </>
   );
