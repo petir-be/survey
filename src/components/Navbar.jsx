c9d76a16fd2d3015e2884ffba2434ca7eefc35cf
mc489 <146339365+mc489@users.noreply.github.com>
2026-02-14 22:28:44 +0800

    initial commit for mcui branch

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,9 +20,9 @@ function NavBar() {
       ></AccountModal>
       {isDesktopOrLaptop && (
         <nav className="absolute w-full z-10">
-          <div className="flex border-2 border-(--dirty-white) justify-between py-8 px-10 ">
+          <div className="flex justify-between py-8 px-10 ">
             <Link to={`/`}>
-              <h1 className="font-zendots text-[30px] px-2">C-MEN</h1>
+              <h1 className="font-zendots text-white   text-[30px] px-2">C-MEN</h1>
             </Link>
             <div className="font-vagrounded w-1/6 z-10  flex align-center justify-end">
               {isAuthenticated ? (
@@ -37,7 +37,7 @@ function NavBar() {
                 <Link className="w-full h-full" to={`login`}>
                   <button
                     className="ring ring-white text-[14px] font-vagrounded font-bold w-full h-full rounded-3xl
-             drop-shadow-md/20 bg-(--white) cursor-pointer"
+             drop-shadow-md/20 text-white bg-black hover:bg-[#1E1E1E]  cursor-pointer"
                   >
                     Get Started
                   </button>
